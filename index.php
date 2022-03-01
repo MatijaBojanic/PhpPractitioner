323f3151d0cccaf39147b5a8dccc8ec117a37408
Matija Bojanic <matija.bojanic@gmail.com>
2022-03-01 18:58:47 +0100

    Separation of concerns, require

--- index.php
+++ index.php
@@ -1,22 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-    <style>
-        header{
-            background: #e3e3e3;
-            padding: 2em;
-            text-align: center;
-        }
+<?php
 
-    </style>
-</head>
-<body>
-    <header>
-        <h1>
-            <?php echo "Hello, ".htmlspecialchars($_GET['name']); ?>
-        </h1>
-    </header>
-</body>
-</html>
+$greeting = 'Hello, world';
+
+require 'index.view.php';
