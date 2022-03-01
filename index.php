40e0d7c6c334289849fac462fd78944dc6b99d84
Matija Bojanic <matija.bojanic@gmail.com>
2022-03-01 22:32:33 +0100

    added class pdo fetch to class

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
-require 'index.view.php';
+require 'Task.php';
+
 
 try{
     $pdo = new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'root','');
@@ -11,4 +12,6 @@ try{
 $statement = $pdo->prepare('select * from todos');
 $statement->execute();
 
-var_dump($statement->fetchAll(PDO::FETCH_OBJ));
+$tasks = $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
+
+require 'index.view.php';
