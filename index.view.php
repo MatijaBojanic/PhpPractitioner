8a46e440aac2dab81e7e1cad2599f2876b159c06
Matija Bojanic <matija.bojanic@gmail.com>
2022-03-01 21:45:49 +0100

    ternary operator

--- index.view.php
+++ index.view.php
@@ -14,6 +14,24 @@
     </style>
 </head>
 <body>
+    <h1>Task For The Day</h1>
+    <ul>
 
+        <li>
+            <strong>Name: </strong> <?= $task['title'];?>
+        </li>
+
+        <li>
+            <strong>Due date: </strong> <?= $task['due'];?>
+        </li>
+
+        <li>
+            <strong>Personal Responsible: </strong> <?= $task['assigned_to'];?>
+        </li>
+
+        <li>
+            <strong>Status: </strong> <?= $task['completed'] ? "Complete" : "Incomplete";?>
+        </li>
+    </ul>
 </body>
 </html>
