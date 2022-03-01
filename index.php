072304a4c8b47de7d280104a332b7c113614de18
Matija Bojanic <matija.bojanic@gmail.com>
2022-03-01 22:36:26 +0100

    Refactor

--- index.php
+++ index.php
@@ -1,17 +1,10 @@
 <?php
 
 require 'Task.php';
+require 'functions.php';
 
+$pdo = connectToDb();
+$tasks = fetchAllTasks($pdo);
 
-try{
-    $pdo = new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'root','');
-} catch(PDOException $e) {
-    die('Could not connect.');
-}
-
-$statement = $pdo->prepare('select * from todos');
-$statement->execute();
-
-$tasks = $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
 
 require 'index.view.php';
