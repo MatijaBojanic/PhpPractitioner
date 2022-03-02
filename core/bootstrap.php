1623bd2e0bc0bf2f84df4048d02a165ea0ba25df
Matija Bojanic <matija.bojanic@gmail.com>
2022-03-02 19:24:26 +0100

    Autoloading with composer

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -2,10 +2,10 @@
 $app = [];
 
 $app['config'] = require 'config.php';
-require 'core/Router.php';
-require 'core/database/Request.php';
-require 'core/database/QueryBuilder.php';
-require 'core/database/Connection.php';
+//require 'core/Router.php';
+//require 'core/database/Request.php';
+//require 'core/database/QueryBuilder.php';
+//require 'core/database/Connection.php';
 
 $app['database'] =  new QueryBuilder(
     Connection::make($app['config']['database'])
--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
+
+require 'vendor/autoload.php';
 $database = require 'core/bootstrap.php';
 
+
 $uri = trim($_SERVER['REQUEST_URI'], '/');
-//var_dump($router->direct($uri));
-//require $router->direct($uri);
 require Router::load('routes.php')
     ->direct(Request::uri(), Request::method());
