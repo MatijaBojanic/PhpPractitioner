806ab96f7d734d8891b768dfd872812b4332a442
Matija Bojanic <matija.bojanic@gmail.com>
2022-03-02 18:16:45 +0100

    Routing update

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,11 +1,12 @@
 <?php
+$app = [];
 
-$config = require 'config.php';
+$app['config'] = require 'config.php';
 require 'core/Router.php';
 require 'core/database/Request.php';
 require 'core/database/QueryBuilder.php';
 require 'core/database/Connection.php';
 
-return new QueryBuilder(
-    Connection::make($config['database'])
+$app['database'] =  new QueryBuilder(
+    Connection::make($app['config']['database'])
 );
--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 $database = require 'core/bootstrap.php';
-//$router = new Router;
-//require 'routes.php';
+
 $uri = trim($_SERVER['REQUEST_URI'], '/');
 //var_dump($router->direct($uri));
 //require $router->direct($uri);
